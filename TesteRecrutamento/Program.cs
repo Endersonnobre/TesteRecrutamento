12d827c04bff7c9946394aad59c58e037708eb0f
Endersonnobre <48106135+Endersonnobre@users.noreply.github.com>
2022-03-25 12:04:18 -0300

    Codificação

--- TesteRecrutamento/Program.cs
+++ TesteRecrutamento/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TesteRecrutamento.src.Entities;
 
 namespace TesteRecrutamento
 {
@@ -6,7 +7,34 @@ namespace TesteRecrutamento
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Quantos números você quer incluir?");
+            int amount = int.Parse(Console.ReadLine());
+
+            FindTarget findTarget = new FindTarget(amount);
+
+            for (int i = 0; i < findTarget.Numbers.Length; i++)
+            {
+                Console.WriteLine($"Digite o {i+1}ª número");
+                findTarget.insertNumber(int.Parse(Console.ReadLine()), i);
+            }
+
+            Console.Clear();
+
+            Console.WriteLine($"Digite o número alvo");
+            int targert = int.Parse(Console.ReadLine());
+
+            string combinations = findTarget.Find(targert);
+
+            if (string.IsNullOrEmpty(combinations))
+            {
+                Console.WriteLine("Não há combinações possíveis que deem o número alvo");
+            }
+            else 
+            {
+                Console.WriteLine($"As combinações que, somados, dão o número alvo são:\n{combinations}");
+            }
+            
+            
         }
     }
 }
---
+++ TesteRecrutamento/src/Entities/FindTarget.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TesteRecrutamento.src.Entities
+{
+    public class FindTarget
+    {
+        public int[] Numbers { get; set; }
+
+        public int Target { get; set; }
+
+        public FindTarget()
+        {
+                
+        }
+
+        public FindTarget(int range)
+        {
+            Numbers = new int[range];
+        }
+
+        public void insertNumber(int number, int pos)
+        {
+            Numbers[pos] = number;
+        }
+
+        public string Find(int target)
+        {
+            string sums = "";
+            
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                int count = 0;
+                while (count < Numbers.Length)
+                {
+                    if(i != count)
+                    {
+                        sums += Numbers[i] + Numbers[count] == target ? $"{Numbers[i]} na posição {i} do array + {Numbers[count]} na posição {count} do array \n" : string.Empty;
+                        count++;
+                    }
+                    else
+                    {
+                        count++;
+                    }
+                }
+            }
+            return sums;
+        }
+    }
+}
